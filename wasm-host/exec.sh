a81ccd9a5d7645436ff9faaf76686bd9cb6b3ba5
Mayukha Vadari <mvadari@gmail.com>
2025-04-17 16:53:52 -0400

    fix exec.sh

--- wasm-host/exec.sh
+++ wasm-host/exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cargo build
-cargo run -- --wasm-file /Users/dfuelling/Development/github/ripple/craft/projects/xrpl_std_example/target/wasm32-unknown-unknown/debug/xrpl_std_example.wasm --test-case success
+cargo run -- --wasm-file ../projects/xrpl_std_example/target/wasm32-unknown-unknown/debug/xrpl_std_example.wasm --test-case success
 
 

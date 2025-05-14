a3a078736601e044f0edc2177d74b6a68d0a4bff
Peng Wang <pwang200@gmail.com>
2025-05-13 21:25:11 -0400

    WIP: updated host_lib to use *u8 as pointer and usize as len

--- xrpl-std/src/host_lib.rs
+++ xrpl-std/src/host_lib.rs
@@ -1,44 +1,43 @@
 #[link(wasm_import_module = "host_lib")]
 extern "C" {
-    pub fn getLedgerSqn() -> i32;
-    pub fn getParentLedgerTime() -> i32;
-    //TODO should we change all ptr to  *mut u8?
-    pub fn get_parent_ledger_hash(out_buff_ptr: i32, out_buff_len: i32) -> i32;
+    pub fn get_ledger_sqn(out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    pub fn get_parent_ledger_time(out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    pub fn get_parent_ledger_hash(out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+
+    pub fn ledger_slot_set(keylet_ptr: * const u8, keylet_len: usize, slot_num: i32) -> i32;
+
+    pub fn get_tx_field(field: i32, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    pub fn get_current_ledger_obj_field(field: i32, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    pub fn get_ledger_obj_field(slot: i32, field: i32, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+
+    pub fn get_tx_nested_field(locator_ptr: * const u8, locator_len: usize, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    pub fn get_current_ledger_obj_nested_field(locator_ptr: * const u8, locator_len: usize, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    pub fn get_ledger_obj_nested_field(slot: i32, locator_ptr: * const u8, locator_len: usize, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+
+    pub fn get_tx_array_len(field: i32) -> i32;
+    pub fn get_current_ledger_obj_array_len(field: i32) -> i32;
+    pub fn get_ledger_obj_array_len(slot: i32, field: i32) -> i32;
+
+    pub fn get_tx_nested_array_len(locator_ptr: * const u8, locator_len: usize) -> i32;
+    pub fn get_current_ledger_obj_nested_array_len(locator_ptr: * const u8, locator_len: usize) -> i32;
+    pub fn get_ledger_obj_nested_array_len(slot: i32, locator_ptr: * const u8, locator_len: usize) -> i32;
+
+    pub fn update_data(data_ptr: * const u8, data_len: usize);
+
+    pub fn compute_sha512_half(data_ptr: * const u8, data_len: usize, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
     
-    pub fn ledger_slot_set(keylet_ptr: i32, keylet_len: i32, slot_num: i32) -> i32;
-
-    pub fn get_tx_field(field: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-    pub fn get_current_ledger_obj_field(field: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-    pub fn get_ledger_obj_field(slot: i32, field: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-
-    pub fn get_tx_nested_field(locator_ptr: i32, locator_len: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-    pub fn get_current_ledger_obj_nested_field(locator_ptr: i32, locator_len: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-    pub fn get_ledger_obj_nested_field(slot: i32, locator_ptr: i32, locator_len: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-
-    pub fn get_tx_field_len(field: i32) -> i32;
-    pub fn get_current_ledger_obj_field_len(field: i32) -> i32;
-    pub fn get_ledger_obj_field_len(slot: i32, field: i32) -> i32;
-
-    pub fn get_tx_nested_field_len(locator_ptr: i32, locator_len: i32) -> i32;
-    pub fn get_current_ledger_obj_nested_field_len(locator_ptr: i32, locator_len: i32) -> i32;
-    pub fn get_ledger_obj_nested_field_len(slot: i32, locator_ptr: i32, locator_len: i32) -> i32;
-    
-    pub fn updateData(data_ptr: i32, data_len: i32);
-
-    pub fn computeSha512HalfHash(data_ptr: i32, data_len: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-    pub fn accountKeylet(account_ptr: i32, account_len: i32, out_buff_ptr: i32, out_buff_len: i32) -> i32;
-    
+    pub fn account_keylet(account_ptr: * const u8, account_len: usize, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
     //TODO the following will be in a separate PR
-    pub fn credentialKeylet(
-        subject_ptr: i32,
-        subject_len: i32,
-        issuer_ptr: i32,
-        issuer_len: i32,
-        cred_type_ptr: i32,
-        cred_type_len: i32,
-    ) -> i32; //TODO add output
-    pub fn escrowKeylet(account_ptr: i32, account_len: i32, sequence: i32) -> i32; //TODO add output
-    pub fn oracleKeylet(account_ptr: i32, account_len: i32, document_id: i32) -> i32; //TODO add output
-    pub fn getNFT(account_ptr: i32, account_len: i32, nft_id_ptr: i32, nft_id_len: i32) -> i32; //TODO add output
-    pub fn print(str_ptr: i32, str_len: i32); //TODO replace with trace
+    pub fn credential_keylet(
+        subject_ptr: i32, subject_len: i32,
+        issuer_ptr: i32, issuer_len: i32,
+        cred_type_ptr: i32, cred_type_len: i32,
+        out_buff_ptr: * mut u8, out_buff_len: usize
+    ) -> i32;
+    pub fn escrow_keylet(account_ptr: * const u8, account_len: usize, sequence: i32, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    pub fn oracle_keylet(account_ptr: * const u8, account_len: usize, document_id: i32, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    
+    pub fn get_NFT(account_ptr: * const u8, account_len: usize, nft_id_ptr: * const u8, nft_id_len: usize, out_buff_ptr: * mut u8, out_buff_len: usize) -> i32;
+    
+    pub fn print(data_ptr: * const u8, data_len: usize); //TODO replace with David's trace
 }
--- xrpl-std/src/lib.rs
+++ xrpl-std/src/lib.rs
@@ -14,7 +14,7 @@ pub type ContractData = [u8; XRPL_CONTRACT_DATA_SIZE];
 pub fn get_tx_account_id() -> Option<AccountID> { //TODO replace with Tx Object
     let mut account_id: AccountID = [0; XRPL_ACCOUNT_ID_SIZE];
     if unsafe {
-        host_lib::get_tx_field(sfield::Account, account_id.as_mut_ptr() as i32, account_id.len() as i32)
+        host_lib::get_tx_field(sfield::Account, account_id.as_mut_ptr(), account_id.len())
     } == XRPL_ACCOUNT_ID_SIZE as i32 {
         Some(account_id)
     } else {
@@ -25,7 +25,7 @@ pub fn get_tx_account_id() -> Option<AccountID> { //TODO replace with Tx Object
 pub fn get_current_escrow_account_id() -> Option<AccountID> { //TODO replace with escrow Object
     let mut account_id: AccountID = [0; XRPL_ACCOUNT_ID_SIZE];
     if unsafe {
-        host_lib::get_current_ledger_obj_field(sfield::Account, account_id.as_mut_ptr() as i32, account_id.len() as i32)
+        host_lib::get_current_ledger_obj_field(sfield::Account, account_id.as_mut_ptr(), account_id.len())
     } == XRPL_ACCOUNT_ID_SIZE as i32 {
         Some(account_id)
     } else {
@@ -36,7 +36,7 @@ pub fn get_current_escrow_account_id() -> Option<AccountID> { //TODO replace wit
 pub fn get_current_escrow_destination() -> Option<AccountID> { //TODO replace with escrow Object
     let mut account_id: AccountID = [0; XRPL_ACCOUNT_ID_SIZE];
     if unsafe {
-        host_lib::get_current_ledger_obj_field(sfield::Destination, account_id.as_mut_ptr() as i32, account_id.len() as i32)
+        host_lib::get_current_ledger_obj_field(sfield::Destination, account_id.as_mut_ptr(), account_id.len())
     } == XRPL_ACCOUNT_ID_SIZE as i32 {
         Some(account_id)
     } else {
@@ -47,7 +47,7 @@ pub fn get_current_escrow_destination() -> Option<AccountID> { //TODO replace wi
 pub fn get_current_escrow_data() -> Option<ContractData> { //TODO replace with escrow Object
     let mut data: ContractData = [0; XRPL_CONTRACT_DATA_SIZE];
     if unsafe {
-        host_lib::get_current_ledger_obj_field(sfield::Data, data.as_mut_ptr() as i32, data.len() as i32)
+        host_lib::get_current_ledger_obj_field(sfield::Data, data.as_mut_ptr(), data.len())
     } == XRPL_ACCOUNT_ID_SIZE as i32 {
         Some(data)
     } else {
@@ -58,7 +58,7 @@ pub fn get_current_escrow_data() -> Option<ContractData> { //TODO replace with e
 pub fn get_current_escrow_finish_after() -> Option<i32> { //TODO replace with escrow Object
     let mut after = 0i32;
     if unsafe {
-        host_lib::get_current_ledger_obj_field(sfield::Data, (&mut after) as *mut i32 as i32, 4)
+        host_lib::get_current_ledger_obj_field(sfield::Data, (&mut after) as *mut i32 as *mut u8, 4)
     } == XRPL_ACCOUNT_ID_SIZE as i32 {
         Some(after)
     } else {
@@ -72,14 +72,14 @@ pub fn get_account_balance(aid: &AccountID) -> Option<u64> { //TODO replace with
         Some(keylet) => { keylet }
     };
     let slot = unsafe {
-        host_lib::ledger_slot_set(keylet.as_ptr() as i32, keylet.len() as i32, 0)
+        host_lib::ledger_slot_set(keylet.as_ptr(), keylet.len(), 0)
     };
     if slot <= 0 {
         return None;
     }
     let mut balance = 0u64;
     if unsafe {
-        host_lib::get_ledger_obj_field(slot, sfield::Balance, (&mut balance) as *mut u64 as i32, 8)
+        host_lib::get_ledger_obj_field(slot, sfield::Balance, (&mut balance) as *mut u64 as *mut u8, 8)
     } == 8 {
         Some(balance)
     } else {
@@ -90,7 +90,7 @@ pub fn get_account_balance(aid: &AccountID) -> Option<u64> { //TODO replace with
 pub fn account_keylet(aid: &AccountID) -> Option<Keylet> {
     let mut key_let: Keylet = [0; XRPL_KEYLET_SIZE];
     if XRPL_KEYLET_SIZE as i32 == unsafe {
-        host_lib::accountKeylet(aid.as_ptr() as i32, aid.len() as i32, key_let.as_mut_ptr() as i32, key_let.len() as i32)
+        host_lib::account_keylet(aid.as_ptr(), aid.len(), key_let.as_mut_ptr(), key_let.len())
     } {
         Some(key_let)
     } else {
@@ -100,6 +100,6 @@ pub fn account_keylet(aid: &AccountID) -> Option<Keylet> {
 
 pub fn update_current_escrow_data(data: ContractData) {
     unsafe {
-        host_lib::updateData(data.as_ptr() as i32, data.len() as i32);
+        host_lib::update_data(data.as_ptr(), data.len());
     }
 }

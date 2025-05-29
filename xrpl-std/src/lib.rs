e8b609c10301ba6ac4558e3b9293d37efc4cbe14
Peng Wang <pwang200@gmail.com>
2025-05-29 11:35:20 -0400

    WIP decoder tests

--- xrpl-std/src/lib.rs
+++ xrpl-std/src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+// #![no_std]
 
 pub mod core;
 pub mod host;
@@ -130,12 +130,12 @@ pub fn update_current_escrow_data(data: ContractData) {
     }
 }
 
-/// This function is called on panic, but only in the WASM architecture. In non-WASM (e.g., in the
-/// Host Simulator) the standard lib is available, which includes a panic handler.
-#[cfg(target_arch = "wasm32")]
-#[panic_handler]
-fn panic(_info: &::core::panic::PanicInfo) -> ! {
-    // This instruction will halt execution of the WASM module.
-    // It's the WASM equivalent of a trap or an unrecoverable error.
-    ::core::arch::wasm32::unreachable();
-}
+// /// This function is called on panic, but only in the WASM architecture. In non-WASM (e.g., in the
+// /// Host Simulator) the standard lib is available, which includes a panic handler.
+// #[cfg(target_arch = "wasm32")]
+// #[panic_handler]
+// fn panic(_info: &::core::panic::PanicInfo) -> ! {
+//     // This instruction will halt execution of the WASM module.
+//     // It's the WASM equivalent of a trap or an unrecoverable error.
+//     ::core::arch::wasm32::unreachable();
+// }

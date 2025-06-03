0b6fc5701b4db800eaf3569b6771486e4dda8e99
David Fuelling <sappenin@gmail.com>
2025-06-03 08:52:55 -0600

    Cleanup

--- projects/notary/src/lib.rs
+++ projects/notary/src/lib.rs
@@ -4,13 +4,13 @@ use xrpl_std::get_tx_account_id;
 // Notary account that is authorized to finish the escrow
 const NOTARY_ACCOUNT: &str = "rPPLRQwB3KGvpfDMABZucA8ifJJcvQhHD3"; // Account 2 (example)
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn finish() -> bool {
     unsafe {
         let tx_account = get_tx_account_id();
 
         // Convert account bytes to string for comparison
-        let tx_account_str = match str::from_utf8(&tx_account) {
+        let tx_account_str = match str::from_utf8(&tx_account.unwrap()) {
             Ok(s) => s,
             Err(_) => return false,
         };

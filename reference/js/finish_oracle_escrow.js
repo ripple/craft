12c581639edc65436c255e618964ae689903988e
Mayukha Vadari <mvadari@gmail.com>
2025-06-03 17:59:22 -0400

    add JS script for it

---
+++ reference/js/finish_oracle_escrow.js
@@ -0,0 +1,103 @@
+const xrpl = require("xrpl")
+
+if (process.argv.length != 6) {
+  console.error(
+    'Usage: ' +
+      process.argv[0] +
+      ' ' +
+      process.argv[1] +
+      ' <Account> <AccountSecret> <Owner> <OfferSequence>',
+  )
+  process.exit(1)
+}
+
+const url = "ws://127.0.0.1:6006"
+const client = new xrpl.Client(url)
+
+const [, , account, accountSecret, owner, offerSequence] = process.argv
+
+async function submit(tx, wallet, debug = true) {
+  const result = await client.submitAndWait(tx, {autofill: true, wallet})
+  console.log("SUBMITTED " + tx.TransactionType)
+  if (debug)
+    console.log(result.result ?? result)
+  else
+    console.log("Result code: " + result.result?.meta?.TransactionResult)
+  return result
+}
+
+async function finishEscrow() {
+  let interval
+  try {
+    await client.connect()
+    console.log(`Connected to ${url}`)
+    await client.request({command: 'ledger_accept'})
+    
+    interval = setInterval(() => {if (client.isConnected()) client.request({command: 'ledger_accept'})},1000)
+
+    const wallet = xrpl.Wallet.fromSeed(accountSecret)
+    
+    // Verify the account matches the wallet
+    if (wallet.address !== account) {
+      console.error("Error: Provided account doesn't match the wallet derived from the secret")
+      process.exit(1)
+    }
+
+    console.log("\nTransaction Details:")
+    console.log(`Account (Finishing Escrow): ${account}`)
+    console.log(`Owner (Created Escrow): ${owner}`)
+    console.log(`Offer Sequence: ${offerSequence}\n`)
+
+    const mintTx = {
+      TransactionType: "OracleSet",
+      Account: account,
+      OracleDocumentID: 1,
+      Provider: xrpl.convertStringToHex("test"),
+      AssetClass: xrpl.convertStringToHex("crypto"),
+      LastUpdateTime: Math.floor(Date.now() / 1000),
+        PriceDataSeries: [
+          {
+            PriceData: {
+              BaseAsset: 'XRP',
+              QuoteAsset: 'USD',
+              AssetPrice: 2,
+              Scale: 1,
+            },
+          },
+        ]
+    }
+    console.log("Creating Oracle...")
+    const mintResponse = await submit(mintTx, wallet)
+    if (mintResponse.result.meta.TransactionResult === "tesSUCCESS") {
+      console.log("\Oracle created successfully!")
+    } else {
+      console.error("\nFailed to finish escrow:", mintResponse.result.meta.TransactionResult)
+    }
+
+    const tx = {
+      TransactionType: 'EscrowFinish',
+      Account: account,
+      Owner: owner,
+      OfferSequence: parseInt(offerSequence),
+      ComputationAllowance: 1000000,
+    }
+
+    console.log("Submitting EscrowFinish transaction...")
+    const response = await submit(tx, wallet)
+    
+    if (response.result.meta.TransactionResult === "tesSUCCESS") {
+      console.log("\nEscrow finished successfully!")
+    } else {
+      console.error("\nFailed to finish escrow:", response.result.meta.TransactionResult)
+    }
+
+  } catch (error) {
+    console.error("Error:", error.message)
+  } finally {
+    await client.disconnect()
+    clearInterval(interval)
+    console.log("Disconnected")
+  }
+}
+
+finishEscrow() 

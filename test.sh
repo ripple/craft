abff05b7de37917af52f3c264fea2c89f5517383
Mayukha Vadari <mvadari@gmail.com>
2025-06-04 17:37:57 -0400

    clean up CI (#49)

--- test.sh
+++
@@ -1,6 +0,0 @@
-for dir in ./projects/*/; do
-    if [ -f "$dir/Cargo.toml" ]; then
-        echo "🔧 Building in $dir"
-        (cd "$dir" && cargo rustc --target wasm32-unknown-unknown -- -D warnings && cargo build --target wasm32-unknown-unknown) || exit 1
-    fi
-done

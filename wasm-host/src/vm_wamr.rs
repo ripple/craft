0a51c651482e4eff4c08c31cc17d6366a37b36dc
pwang200 <354723+pwang200@users.noreply.github.com>
2025-06-27 15:28:36 -0400

    add host function and instruction cost metering in wamr and craft
    * use wamr in ripple repo, add host function and instruction cost metering
    * remove unused wamr-rust-sdk in Cargo.toml
    * use wamr by default

--- wasm-host/src/vm_wamr.rs
+++ wasm-host/src/vm_wamr.rs
@@ -21,36 +21,36 @@ use wamr_rust_sdk::value::WasmValue;
 
 #[rustfmt::skip]
 #[allow(unused)]
-pub fn run_func(wasm_file: String, func_name: &str, data_source: MockData) -> Result<bool, RuntimeError>{
+pub fn run_func(wasm_file: String, func_name: &str, gas_cap: Option<u32>, data_source: MockData) -> Result<bool, RuntimeError>{
     debug!("Setting up wamr runtime and registering host functions");
     let mut data_provider = DataProvider::new(data_source);
     let runtime = Runtime::builder()
         .use_system_allocator()
-        .register_host_function("get_ledger_sqn", get_ledger_sqn as *mut c_void, "(*~)i", data_provider.as_ptr())
-        .register_host_function("get_parent_ledger_time", get_parent_ledger_time as *mut c_void, "(*~)i", data_provider.as_ptr())
-        .register_host_function("get_parent_ledger_hash", get_parent_ledger_hash as *mut c_void, "(*~)i", data_provider.as_ptr())
-        .register_host_function("cache_ledger_obj", cache_ledger_obj as *mut c_void, "(*~i)i", data_provider.as_ptr())
-        .register_host_function("get_tx_field", get_tx_field as *mut c_void, "(i*~)i", data_provider.as_ptr())
-        .register_host_function("get_current_ledger_obj_field", get_current_ledger_obj_field as *mut c_void, "(i*~)i", data_provider.as_ptr())
-        .register_host_function("get_ledger_obj_field", get_ledger_obj_field as *mut c_void, "(ii*~)i", data_provider.as_ptr())
-        .register_host_function("get_tx_nested_field", get_tx_nested_field as *mut c_void, "(*~*~)i", data_provider.as_ptr())
-        .register_host_function("get_current_ledger_obj_nested_field", get_current_ledger_obj_nested_field as *mut c_void, "(*~*~)i", data_provider.as_ptr())
-        .register_host_function("get_ledger_obj_nested_field", get_ledger_obj_nested_field as *mut c_void, "(i*~*~)i", data_provider.as_ptr())
-        .register_host_function("get_tx_array_len", get_tx_array_len as *mut c_void, "(i)i", data_provider.as_ptr())
-        .register_host_function("get_current_ledger_obj_array_len", get_current_ledger_obj_array_len as *mut c_void, "(i)i", data_provider.as_ptr())
-        .register_host_function("get_ledger_obj_array_len", get_ledger_obj_array_len as *mut c_void, "(ii)i", data_provider.as_ptr())
-        .register_host_function("get_tx_nested_array_len", get_tx_nested_array_len as *mut c_void, "(*~)i", data_provider.as_ptr())
-        .register_host_function("get_current_ledger_obj_nested_array_len", get_current_ledger_obj_nested_array_len as *mut c_void, "(*~)i", data_provider.as_ptr())
-        .register_host_function("get_ledger_obj_nested_array_len", get_ledger_obj_nested_array_len as *mut c_void, "(i*~)i", data_provider.as_ptr())
-        .register_host_function("update_data", update_data as *mut c_void, "(*~)i", data_provider.as_ptr())
-        .register_host_function("compute_sha512_half", compute_sha512_half as *mut c_void, "(*~*~)i", data_provider.as_ptr())
-        .register_host_function("account_keylet", account_keylet as *mut c_void, "(*~*~)i", data_provider.as_ptr())
-        .register_host_function("credential_keylet", credential_keylet as *mut c_void, "(*~*~*~*~)i", data_provider.as_ptr())
-        .register_host_function("escrow_keylet", escrow_keylet as *mut c_void, "(*~i*~)i", data_provider.as_ptr())
-        .register_host_function("oracle_keylet", oracle_keylet as *mut c_void, "(*~i*~)i", data_provider.as_ptr())
-        .register_host_function("get_NFT", get_nft as *mut c_void, "(*~*~*~)i", data_provider.as_ptr())
-        .register_host_function("trace", trace as *mut c_void, "(*~*~i)i", data_provider.as_ptr())
-        .register_host_function("trace_num", trace_num as *mut c_void, "(*~I)i", data_provider.as_ptr())
+        .register_host_function("get_ledger_sqn", get_ledger_sqn as *mut c_void, "(*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_parent_ledger_time", get_parent_ledger_time as *mut c_void, "(*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_parent_ledger_hash", get_parent_ledger_hash as *mut c_void, "(*~)i", 30, data_provider.as_ptr())
+        .register_host_function("cache_ledger_obj", cache_ledger_obj as *mut c_void, "(*~i)i", 1000, data_provider.as_ptr())
+        .register_host_function("get_tx_field", get_tx_field as *mut c_void, "(i*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_current_ledger_obj_field", get_current_ledger_obj_field as *mut c_void, "(i*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_ledger_obj_field", get_ledger_obj_field as *mut c_void, "(ii*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_tx_nested_field", get_tx_nested_field as *mut c_void, "(*~*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_current_ledger_obj_nested_field", get_current_ledger_obj_nested_field as *mut c_void, "(*~*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_ledger_obj_nested_field", get_ledger_obj_nested_field as *mut c_void, "(i*~*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_tx_array_len", get_tx_array_len as *mut c_void, "(i)i", 30, data_provider.as_ptr())
+        .register_host_function("get_current_ledger_obj_array_len", get_current_ledger_obj_array_len as *mut c_void, "(i)i", 30, data_provider.as_ptr())
+        .register_host_function("get_ledger_obj_array_len", get_ledger_obj_array_len as *mut c_void, "(ii)i", 30, data_provider.as_ptr())
+        .register_host_function("get_tx_nested_array_len", get_tx_nested_array_len as *mut c_void, "(*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_current_ledger_obj_nested_array_len", get_current_ledger_obj_nested_array_len as *mut c_void, "(*~)i", 30, data_provider.as_ptr())
+        .register_host_function("get_ledger_obj_nested_array_len", get_ledger_obj_nested_array_len as *mut c_void, "(i*~)i", 30, data_provider.as_ptr())
+        .register_host_function("update_data", update_data as *mut c_void, "(*~)i", 1000, data_provider.as_ptr())
+        .register_host_function("compute_sha512_half", compute_sha512_half as *mut c_void, "(*~*~)i", 200, data_provider.as_ptr())
+        .register_host_function("account_keylet", account_keylet as *mut c_void, "(*~*~)i", 200, data_provider.as_ptr())
+        .register_host_function("credential_keylet", credential_keylet as *mut c_void, "(*~*~*~*~)i", 200, data_provider.as_ptr())
+        .register_host_function("escrow_keylet", escrow_keylet as *mut c_void, "(*~i*~)i", 200, data_provider.as_ptr())
+        .register_host_function("oracle_keylet", oracle_keylet as *mut c_void, "(*~i*~)i", 200, data_provider.as_ptr())
+        .register_host_function("get_NFT", get_nft as *mut c_void, "(*~*~*~)i", 30, data_provider.as_ptr())
+        .register_host_function("trace", trace as *mut c_void, "(*~*~i)i", 30, data_provider.as_ptr())
+        .register_host_function("trace_num", trace_num as *mut c_void, "(*~I)i", 30, data_provider.as_ptr())
         .build()?;
 
     debug!("Loading WASM module from file: {}", wasm_file);
@@ -60,12 +60,24 @@ pub fn run_func(wasm_file: String, func_name: &str, data_source: MockData) -> Re
 
     debug!("Executing WASM function: {}", func_name);
     let func = Function::find_export_func(&instance, "finish")?;
-    let results = func.call(&instance, &vec![])?;
-    if let Some(WasmValue::I32(r1)) = results.first() {
-        info!("run_func result: {}", r1);
-        Ok(*r1 == 1)
-    } else {
-        warn!("Unexpected run_func result: {:?}", results);
-        Ok(false)
+    let gas_begin = gas_cap.map_or(0, |x|x);
+    let results = func.call(&instance, &vec![], gas_cap)?;
+    match results {
+        (rv, gas_end) if rv.len() == 1 => {
+            if let WasmValue::I32(r1) = rv[0] {
+                info!("run_func result: {}", r1);
+                if gas_begin > 0 {
+                    info!("run_func gas cost: {}", gas_begin - gas_end);
+                }
+                Ok(r1 == 1)
+            } else {
+                warn!("Unexpected run_func result vec: {:?}", rv[0]);
+                Ok(false)
+            }
+        }
+        _ => {
+            warn!("run_func error: {:?}", results);
+            Ok(false)
+        }
     }
 }

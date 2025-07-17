0da0ffb1a1b1a1394b3be37390f1b5dec714ae71
Mayukha Vadari <mvadari@gmail.com>
2025-07-17 12:24:27 -0400

    update script to support Github

--- tools/compareHostFunctions.js
+++ tools/compareHostFunctions.js
@@ -13,150 +13,182 @@ if (process.argv.length != 3) {
 //  Get all necessary files from rippled
 ////////////////////////////////////////////////////////////////////////
 const path = require('path')
+const fs = require('fs/promises')
 
-const fs = require('fs')
-function readFile(filename) {
-  try {
-    return fs.readFileSync(filename).toString('utf-8')
-  } catch (err) {
-    console.error(`Error reading file ${filename}:`, err.message)
-    process.exit(1)
-  }
+async function readFileFromGitHub(repo, filename) {
+    if (!repo.includes('tree')) {
+        repo += '/tree/HEAD'
+    }
+    let url = repo.replace('github.com', 'raw.githubusercontent.com')
+    url = url.replace('tree/', '')
+    url += '/' + filename
+
+    if (!url.startsWith('http')) {
+        url = 'https://' + url
+    }
+
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(`${response.status} ${response.statusText}`)
+        }
+        return await response.text()
+    } catch (e) {
+        console.error(`Error reading ${url}: ${e.message}`)
+        process.exit(1)
+    }
 }
 
-const wasmImportFile = readFile(
-  path.join(process.argv[2], '/src/xrpld/app/misc/WasmVM.cpp'),
-)
-const hostWrapperFile = readFile(
-  path.join(process.argv[2], '/src/xrpld/app/misc/WasmHostFuncWrapper.h'),
-)
-const rustHostFunctionFile = readFile(path.join(__dirname, '../xrpl-std/src/host/host_bindings.rs'))
-
-let importHits = [
-  ...wasmImportFile.matchAll(
-    /^ *WASM_IMPORT_FUNC2? *\(i, *([A-Za-z]+), *("([A-Za-z_]+)",)? *hfs, *[0-9]+\);$/gm,
-  ),
-]
-const imports = importHits.map((hit) => [hit[1], hit[3] != null ? hit[3] : hit[1]]).filter(
-  (hit) => hit[0] != 'getLedgerSqnOld')
-
-let wrapperHits = [
-  ...hostWrapperFile.matchAll(
-    /^ *using ([A-Za-z]+)_proto =[ \n]*([A-Za-z0-9_]+)\(([A-Za-z0-9_\* \n,]+)\);$/gm,
-  ),
-]
-const wrappers = wrapperHits.map((hit) => [hit[1], hit[2], hit[3].split(',').map((s) => s.trim())])
-if (imports.length != wrappers.length) {
-  console.error(
-    'Imports and Host Functions do not match in length! ' +
-      imports.length +
-      ' != ' +
-      wrappers.length,
-  )
-  process.exit(1)
+async function readFile(folder, filename) {
+    const filePath = path.join(folder, filename)
+    try {
+        return await fs.readFile(filePath, 'utf-8')
+    } catch (e) {
+        throw new Error(`File not found: ${filePath}, ${e.message}`)
+    }
 }
 
-for (let i = 0; i < imports.length; i++) {
-  if (imports[i][0] != wrappers[i][0]) {
+const read = process.argv[2].includes('github.com')
+    ? readFileFromGitHub
+    : readFile
+
+async function main() {
+    const wasmImportFile = await read(
+        process.argv[2], 'src/xrpld/app/misc/WasmVM.cpp',
+    )
+    const hostWrapperFile = await read(
+        process.argv[2], 'src/xrpld/app/misc/WasmHostFuncWrapper.h',
+    )
+    const rustHostFunctionFile = await readFile(__dirname, '../xrpl-std/src/host/host_bindings.rs')
+
+    let importHits = [
+    ...wasmImportFile.matchAll(
+        /^ *WASM_IMPORT_FUNC2? *\(i, *([A-Za-z]+), *("([A-Za-z_]+)",)? *hfs, *[0-9]+\);$/gm,
+    ),
+    ]
+    const imports = importHits.map((hit) => [hit[1], hit[3] != null ? hit[3] : hit[1]]).filter(
+    (hit) => hit[0] != 'getLedgerSqnOld')
+
+    let wrapperHits = [
+    ...hostWrapperFile.matchAll(
+        /^ *using ([A-Za-z]+)_proto =[ \n]*([A-Za-z0-9_]+)\(([A-Za-z0-9_\* \n,]+)\);$/gm,
+    ),
+    ]
+    const wrappers = wrapperHits.map((hit) => [hit[1], hit[2], hit[3].split(',').map((s) => s.trim())])
+    if (imports.length != wrappers.length) {
     console.error(
-      'Imports and Host Functions do not match at index ' +
-        i +
-        ': ' +
-        imports[i][0] +
+        'Imports and Host Functions do not match in length! ' +
+        imports.length +
         ' != ' +
-        wrappers[i][0],
+        wrappers.length,
     )
     process.exit(1)
-  }
-}
-
-const cppHostFunctions = imports.map((hit, i) => {
-  return {
-    name: hit[1],
-    return: wrappers[i][1],
-    params: wrappers[i][2],
-  }
-})
-
-const paramTranslation = {
-    'i32': 'int32_t',
-    'u32': 'uint32_t',
-    'usize': 'int32_t',
-    'i64': 'int64_t',
-    '*const u8': 'uint8_t const*',
-    '*mut u8': 'uint8_t*',
-}
-
-function translateParamType(param) {
-  if (param in paramTranslation) {
-    return paramTranslation[param]
-  }
-  console.error(`Unknown parameter type: ${param}`)
-  process.exit(1)
-}
-
-let rustHits = [
-  ...rustHostFunctionFile.matchAll(
-    /^ *pub fn ([A-Za-z_]+)\([ \n]*([A-Za-z0-9_:*, \n]+)\) -> ([A-Za-z0-9]+);$/gm,
-  ),
-]
-const rustFuncs = rustHits.map((hit) => [hit[1], hit[3], hit[2].trim().split(',').map((s) => s.trim()).filter((s) => s.length > 0).map((s) => s.split(':')[1].trim())])
-const rustHostFunctions = rustFuncs.map((hit) => {
-  return {
-    name: hit[0],
-    return: translateParamType(hit[1]),
-    params: hit[2].map(translateParamType),
-  }
-})
-
-if (rustHostFunctions.length != cppHostFunctions.length) {
-  console.error(
-    'Rust Host Functions and Host Functions do not match in length! ' +
-      rustHostFunctions.length +
-      ' != ' + cppHostFunctions.length
-    )
-    if (rustHostFunctions.length < cppHostFunctions.length) {
-        const missing = cppHostFunctions.filter(f => !rustHostFunctions.some(rf => rf.name === f[0]))
-        console.error('Missing Rust Host Functions:', missing.map(f => f.name).join(', '))
-    } else {
-        const missing = rustHostFunctions.filter(f => !cppHostFunctions.some(rf => rf[0] === f.name))
-        console.error('Missing Host Functions:', missing.map(f => f.name).join(', '))
     }
+
+    for (let i = 0; i < imports.length; i++) {
+    if (imports[i][0] != wrappers[i][0]) {
+        console.error(
+        'Imports and Host Functions do not match at index ' +
+            i +
+            ': ' +
+            imports[i][0] +
+            ' != ' +
+            wrappers[i][0],
+        )
+        process.exit(1)
+    }
+    }
+
+    const cppHostFunctions = imports.map((hit, i) => {
+    return {
+        name: hit[1],
+        return: wrappers[i][1],
+        params: wrappers[i][2],
+    }
+    })
+
+    const paramTranslation = {
+        'i32': 'int32_t',
+        'u32': 'uint32_t',
+        'usize': 'int32_t',
+        'i64': 'int64_t',
+        '*const u8': 'uint8_t const*',
+        '*mut u8': 'uint8_t*',
+    }
+
+    function translateParamType(param) {
+    if (param in paramTranslation) {
+        return paramTranslation[param]
+    }
+    console.error(`Unknown parameter type: ${param}`)
     process.exit(1)
+    }
+
+    let rustHits = [
+    ...rustHostFunctionFile.matchAll(
+        /^ *pub fn ([A-Za-z_]+)\([ \n]*([A-Za-z0-9_:*, \n]+)\) -> ([A-Za-z0-9]+);$/gm,
+    ),
+    ]
+    const rustFuncs = rustHits.map((hit) => [hit[1], hit[3], hit[2].trim().split(',').map((s) => s.trim()).filter((s) => s.length > 0).map((s) => s.split(':')[1].trim())])
+    const rustHostFunctions = rustFuncs.map((hit) => {
+    return {
+        name: hit[0],
+        return: translateParamType(hit[1]),
+        params: hit[2].map(translateParamType),
+    }
+    })
+
+    if (rustHostFunctions.length != cppHostFunctions.length) {
+    console.error(
+        'Rust Host Functions and Host Functions do not match in length! ' +
+        rustHostFunctions.length +
+        ' != ' + cppHostFunctions.length
+        )
+        if (rustHostFunctions.length < cppHostFunctions.length) {
+            const missing = cppHostFunctions.filter(f => !rustHostFunctions.some(rf => rf.name === f[0]))
+            console.error('Missing Rust Host Functions:', missing.map(f => f.name).join(', '))
+        } else {
+            const missing = rustHostFunctions.filter(f => !cppHostFunctions.some(rf => rf[0] === f.name))
+            console.error('Missing Host Functions:', missing.map(f => f.name).join(', '))
+        }
+        process.exit(1)
+    }
+
+    let hasError = false
+    rustHostFunctions.forEach((hit, index) => {
+        if (hit.name != cppHostFunctions[index].name) {
+            console.error(
+                `Rust Host Function name mismatch: ${hit.name} != ${cppHostFunctions[index].name}`,
+            )
+            hasError = true
+        }
+        else if (hit.return != cppHostFunctions[index].return) {
+            console.error(
+                `Rust Host Function return type mismatch for ${hit.name}: ${hit.return} != ${cppHostFunctions[index].return}`,
+            )
+            hasError = true
+        }
+        else if (hit.params.length != cppHostFunctions[index].params.length) {
+            console.error(
+                `Rust Host Function parameter count mismatch for ${hit.name}: ${hit.params.length} != ${cppHostFunctions[index].params.length}`,
+            )
+            hasError = true
+        } else {
+            hit.params.forEach((param, paramIndex) => {
+                if (param != cppHostFunctions[index].params[paramIndex]) {
+                    console.error(
+                        `Rust Host Function parameter type mismatch for ${hit.name}, parameter ${paramIndex}: ${param} != ${cppHostFunctions[index].params[paramIndex]}`,
+                    )
+                    hasError = true
+                }
+            })
+        }
+    })
+    if (hasError) {
+    process.exit(1)
+    }
+
+    console.log('All host functions match between Rust and C++ implementations.')
 }
 
-let hasError = false
-rustHostFunctions.forEach((hit, index) => {
-    if (hit.name != cppHostFunctions[index].name) {
-        console.error(
-            `Rust Host Function name mismatch: ${hit.name} != ${cppHostFunctions[index].name}`,
-        )
-        hasError = true
-    }
-    else if (hit.return != cppHostFunctions[index].return) {
-        console.error(
-            `Rust Host Function return type mismatch for ${hit.name}: ${hit.return} != ${cppHostFunctions[index].return}`,
-        )
-        hasError = true
-    }
-    else if (hit.params.length != cppHostFunctions[index].params.length) {
-        console.error(
-            `Rust Host Function parameter count mismatch for ${hit.name}: ${hit.params.length} != ${cppHostFunctions[index].params.length}`,
-        )
-        hasError = true
-    } else {
-        hit.params.forEach((param, paramIndex) => {
-            if (param != cppHostFunctions[index].params[paramIndex]) {
-                console.error(
-                    `Rust Host Function parameter type mismatch for ${hit.name}, parameter ${paramIndex}: ${param} != ${cppHostFunctions[index].params[paramIndex]}`,
-                )
-                hasError = true
-            }
-        })
-    }
-})
-if (hasError) {
-  process.exit(1)
-}
-
-console.log('All host functions match between Rust and C++ implementations.')
+main()

fdae9261a63ba35b8e7e0a119389e035fbbaa6e4
David Fuelling <sappenin@gmail.com>
2025-08-26 13:02:31 -0600

    Update for newlines
    * Use printf for better portability

--- build.sh
+++ build.sh
@@ -9,84 +9,73 @@ if [ "$PROFILE" != "dev" ]; then
   PROFILE_FLAG="--profile $PROFILE"
 fi
 
-echo ""
-echo "🔧 Building ALL with profile: $PROFILE"
-echo ""
 
-echo "🔧 Building 'craft' ($PROFILE)"
+printf "🔧 Building ALL with profile: $PROFILE \n"
+
+printf "🔧 Building 'craft' ($PROFILE) \n"
 cargo build $PROFILE_FLAG
 cargo test $PROFILE_FLAG
 cargo clippy --all-targets --all-features
 cargo fmt --all -- --check
-echo ""
-echo "✅ 'Craft' project built successfully"
-echo ""
+
+printf "\n✅ 'Craft' project built successfully\n\n"
+
 
 cd xrpl-std || exit
-echo "🔧 Building 'xrpl-std' ($PROFILE)"
+printf "🔧 Building 'xrpl-std' ($PROFILE) \n"
 cargo build $PROFILE_FLAG
 cargo test $PROFILE_FLAG
 cargo build $PROFILE_FLAG --target wasm32-unknown-unknown
 cargo clippy --all-targets --all-features
 cargo fmt --all -- --check
-echo ""
-echo "✅ 'xrpl-std' project built successfully"
-echo ""
+
+printf "\n✅ 'xrpl-std' project built successfully\n\n"
 
 cd ..
 cd ./wasm-host || exit
-echo "🔧 Building 'wasm-host' ($PROFILE)"
+printf "🔧 Building 'wasm-host' ($PROFILE) \n"
 cargo build $PROFILE_FLAG
 cargo test $PROFILE_FLAG
 cargo clippy --all-targets --all-features
 cargo fmt --all -- --check
-echo ""
-echo "✅  'wasm-host' project built successfully"
-echo ""
+
+printf "\n✅  'wasm-host' project built successfully\n\n"
 
 cd .. || exit
 
-echo "🔧 Setting NOTARY_ACCOUNT_R to rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+printf "🔧 Setting NOTARY_ACCOUNT_R to rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh\n\n"
 export NOTARY_ACCOUNT_R=rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh
 
 find ./projects -name "Cargo.toml" -type f | while read -r cargo_file; do
   dir=$(dirname "$cargo_file")
-  echo "🔧 Building example WASM: $dir"
+  printf "🔧 Building example WASM: $dir \n"
   (cd "$dir" && cargo build $PROFILE_FLAG --target wasm32-unknown-unknown) || exit 1
 done
 
-echo ""
-echo "✅  All WASM examples built successfully"
-echo ""
+printf "\n✅  All WASM examples built successfully\n\n"
 
 find ./projects -name "Cargo.toml" -type f | while read -r cargo_file; do
   dir=$(dirname "$cargo_file")
-  echo "🔧 Building example Rust: $dir"
+  printf "🔧 Building example Rust: $dir \n"
   (cd "$dir" && cargo build $PROFILE_FLAG) || exit 1
 done
 
-echo ""
-echo "✅  All Rust examples built successfully"
-echo ""
+printf "\n✅  All Rust examples built successfully\n\n"
 
 find ./projects -name "Cargo.toml" -type f | while read -r cargo_file; do
   dir=$(dirname "$cargo_file")
-  echo "🔧 cargo fmt for $dir"
+  printf "🔧 cargo fmt for $dir \n"
   (cd "$dir" && cargo fmt --all -- --check) || exit 1
 done
 
-echo ""
-echo "✅  All 'cargo fmt' checks completed successfully"
-echo ""
+printf "\n✅  All 'cargo fmt' checks completed successfully\n\n"
 
 find ./projects -name "Cargo.toml" -type f | while read -r cargo_file; do
   dir=$(dirname "$cargo_file")
-  echo "🔧 'cargo clippy' for $dir"
+  printf "🔧 'cargo clippy' for $dir"
   (cd "$dir" && cargo clippy --all-targets --all-features -- -Dclippy::all) || exit 1
 done
 
-echo ""
-echo "✅  All 'cargo clippy' checks completed successfully"
-echo ""
+printf "\n✅  All 'cargo clippy' checks completed successfully\n\n"
 
 cd ../.. || exit
